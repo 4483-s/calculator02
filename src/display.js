640a1d1645403452a79a863a6cef54888d7a8e5e
Hermann <188955756+4483-s@users.noreply.github.com>
2025-07-08 13:07:55 +0800

    module

---
+++ src/display.js
@@ -0,0 +1,11 @@
+const display = document.querySelector("display");
+function append(x) {
+  display.textContent += x;
+}
+function calcAndDisplay(x) {
+  display.textContent = x;
+}
+function clearAll() {
+  display.textContent = "0";
+}
+export { append, calcAndDisplay, clearAll };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./styles.css"
+// import "./styles.css"
+import { append, calcAndDisplay, clearAll } from "./display.js";
+document.body.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    console.log("hello");
+  }
+});
